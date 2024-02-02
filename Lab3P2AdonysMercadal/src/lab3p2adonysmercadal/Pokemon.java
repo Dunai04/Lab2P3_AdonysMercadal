64f62494f803c025cc0282308abbe8da1ce9de82
Dunai04 <adonysmercadal5@unitec.edu>
2024-02-02 14:44:28 -0600

    1 push

---
+++ Lab3P2AdonysMercadal/src/lab3p2adonysmercadal/Pokemon.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3p2adonysmercadal;
+
+/**
+ *
+ * @author adony
+ */
+public class Pokemon {
+    String nombre;
+    int numPoke;
+    String naturaleza;
+    boolean atrapado = false;
+    protected String pokeBola;
+    
+}
